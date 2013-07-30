3caa75dfa0680e8e32f25de541867fc3935a8cc6
Alexey Bobyrev <alexey.bobyrev@gmail.com>
2013-07-31 01:14:35 +0300

    Use xhr instead of jsonp for GET requests in lastfm api lib

--- js/lastfm/api.js
+++ js/lastfm/api.js
@@ -87,80 +87,18 @@ function LastFM(options){
       /* Close iframe document. */
       doc.close();
     }
-    /* Cross-domain GET request (JSONP). */
     else{
-      /* Get JSONP callback name. */
-      var jsonp = 'jsonp' + new Date().getTime();
-
-      /* Calculate cache hash. */
-      var hash = auth.getApiSignature(params);
-
-      /* Check cache. */
-      if(typeof(cache) != 'undefined' && cache.contains(hash) && !cache.isExpired(hash)){
-        if(typeof(callbacks.success) != 'undefined'){
-          callbacks.success(cache.load(hash));
-        }
-
-        return;
-      }
-
-      /* Set callback name and response format. */
-      params.callback = jsonp;
-      params.format   = 'json';
-
-      /* Create JSONP callback function. */
-      window[jsonp] = function(data){
-        /* Is a cache available?. */
-        if(typeof(cache) != 'undefined'){
-          var expiration = cache.getExpirationTime(params);
-
-          if(expiration > 0){
-            cache.store(hash, data, expiration);
-          }
-        }
-
-        /* Call user callback. */
-        if(typeof(data.error) != 'undefined'){
-          if(typeof(callbacks.error) != 'undefined'){
-            callbacks.error(data.error, data.message);
-          }
-        }
-        else if(typeof(callbacks.success) != 'undefined'){
-          callbacks.success(data);
-        }
-
-        /* Garbage collect. */
-        window[jsonp] = undefined;
-
-        try{
-          delete window[jsonp];
-        }
-        catch(e){
-          /* Nothing. */
-        }
-
-        /* Remove script element. */
-        if(head){
-          head.removeChild(script);
-        }
-      };
-
-      /* Create script element to load JSON data. */
-      var head   = document.getElementsByTagName("head")[0];
-      var script = document.createElement("script");
-
-      /* Build parameter string. */
-      var array = [];
-
+      callbacks['success'] = callbacks['success'] || function(){};
+      callbacks['error'] = callbacks['error'] || function(){};
+      var paramsArray = [];
       for(var param in params){
-        array.push(encodeURIComponent(param) + "=" + encodeURIComponent(params[param]));
+        paramsArray.push(encodeURIComponent(param) + "=" + encodeURIComponent(params[param]));
       }
-
-      /* Set script source. */
-      script.src = apiUrl + '?' + array.join('&').replace(/%20/g, '+');
-
-      /* Append script element. */
-      head.appendChild(script);
+      new XHRequest({
+        url: apiUrl + '?' + paramsArray.join('&').replace(/%20/g, '+'),
+        success: callbacks.success,
+        error: callbacks.error
+      });
     }
   };
 
